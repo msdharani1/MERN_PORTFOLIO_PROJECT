93add1115e9fee41f1bcd7a744b826ed88b5265a
msdharani1 <msdharaniofficial@gmail.com>
2025-09-17 22:03:29 +0530

    Add the Frame motion lib

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { AnimatePresence, motion } from "framer-motion";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import About from "./components/About";
@@ -6,17 +7,32 @@ import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 
 function App() {
+  const location = useLocation();
+
   return (
     <div>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<PageWrapper><Home /></PageWrapper>} />
+          <Route path="/about" element={<PageWrapper><About /></PageWrapper>} />
+          <Route path="/projects" element={<PageWrapper><Projects /></PageWrapper>} />
+          <Route path="/contact" element={<PageWrapper><Contact /></PageWrapper>} />
+        </Routes>
+      </AnimatePresence>
     </div>
   );
 }
+
+const PageWrapper = ({ children }) => (
+  <motion.div
+    initial={{ opacity: 0, y: 0 }}
+    animate={{ opacity: 1, y  : 0 }}
+    exit={{ opacity: 0, y: -50 }}
+    transition={{ duration: 0.4 }}
+  >
+    {children}
+  </motion.div>
+);
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,13 +5,15 @@ const Header = () => {
   return (
     <header className="h-22 w-full bg-[#0B3037] flex justify-between items-center px-5">
       <div>
-        <h1 className="text-white text-2xl font-bold"><Link to="/">MS Dharani</Link></h1>
+        <h1 className="text-white text-2xl font-bold">
+          <Link to="/">MS Dharani</Link>
+        </h1>
       </div>
       <nav className="text-white flex gap-5">
-        <Link to="/"><i class="fa-solid fa-house pr-6"></i>Home</Link>
-        <Link to="/about"><i class="fa-solid fa-user pr-6"></i>About</Link>
-        <Link to="/projects"><i class="fa-solid fa-code pr-6"></i>Projects</Link>
-        <Link to="/contact"><i class="fa-solid fa-message pr-6"></i>Contact</Link>
+        <Link to="/"><i className="fa-solid fa-house pr-2"></i>Home</Link>
+        <Link to="/about"><i className="fa-solid fa-user pr-2"></i>About</Link>
+        <Link to="/projects"><i className="fa-solid fa-code pr-2"></i>Projects</Link>
+        <Link to="/contact"><i className="fa-solid fa-message pr-2"></i>Contact</Link>
       </nav>
     </header>
   );
